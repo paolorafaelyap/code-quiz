c46eecfe7003e041975134eec67d29b98cf94976
paolorafaelyap <paolorafaelyap@gmail.com>
2020-10-22 15:46:13 -0700

    updated html and assets

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// first create a functioning start button
+// when the start button is pressed, timer should start
+// while timer is on, page should ask you questions
+// if input is correct, display message will say 'correct'
+// if input is incorrect, display message will say 'wrong'
+// store points for each correct answer
+// at the end of the quiz, prompt for initials (use submit button)
+// once initials are submitted, create a list for submitted high score
+// select buttons for starting over and clearing high score
+
