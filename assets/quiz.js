a0c8c63eb2bb0b270f40bcf980d188d7afdd1e15
paolorafaelyap <paolorafaelyap@gmail.com>
2020-10-23 18:52:07 -0700

    updated quiz.js and updated quiz.html

---
+++ assets/quiz.js
@@ -0,0 +1,12 @@
+//setting a constant that holds an array for all multiple choice questions
+const quiz = [
+    {
+        question: "Who had the high ground over Anakin?",
+        answers:{a: "Jar-Jar Binks", b: "Shaquille O'Neal", 
+        c: "Obi-Wan Kenobi", d: "X-Wing @Aliciousness"},
+        correctAnswer: "c";
+    },
+    {
+        question: "Which"
+    }
+]
