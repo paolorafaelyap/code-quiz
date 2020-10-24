5f94b301407f5df41de4116067bceaa9813f4e8a
paolorafaelyap <paolorafaelyap@gmail.com>
2020-10-23 18:25:41 -0700

    updated assets and quiz.html

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,21 @@
 // first create a functioning start button (done)
 // when the start button is pressed, index takes us to the page with timer (done)
+
 // when new page opens, timer immediately should start
+    //need to make a javascript page that creates a timer function
+    //initially starts at 75 and subtracts by one at an interval of 1 second
+    //when time runs out, all other questions will be considered unanswered.
+
 // while timer is on, page should ask you questions
+    //This will be performed under a different function.
+
 // if input is correct, display message will say 'correct'
 // if input is incorrect, display message will say 'wrong'
-// store points for each correct answer
+    // the answers, should be stored.
+        // each correct answer should count as 5 points.
+        // each incorrect answer should count as 0 points.
+        
 // at the end of the quiz, prompt for initials (use submit button)
-// once initials are submitted, create a list for submitted high score
+    // initials and score will be stored on a list
 // select buttons for starting over and clearing high score
 
