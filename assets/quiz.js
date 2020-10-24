63dbfc60054aaf835da31bd6b819a2aa9d8c4cf2
paolorafaelyap <paolorafaelyap@gmail.com>
2020-10-23 22:40:55 -0700

    updated quiz.js and html

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,12 +1,51 @@
+//this javascript will show the questions and choices on quiz.html
+// need to create a function for the timer
+// when this page is opened, the timer starts counting
+// another objective is to make sure timer doesn't restart when refreshing
+
+//function for countdown timer
+var countDownTime = 76;
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = 1;
+
+  // Find the distance between now and the count down date
+  var seconds = countDownTime - now;
+
+  
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (seconds < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
 //setting a constant that holds an array for all multiple choice questions
 const quiz = [
     {
         question: "Who had the high ground over Anakin?",
-        answers:{a: "Jar-Jar Binks", b: "Shaquille O'Neal", 
-        c: "Obi-Wan Kenobi", d: "X-Wing @Aliciousness"},
-        correctAnswer: "c";
+        choices:{
+            a: "Jar-Jar Binks", b: "Shaquille O'Neal", 
+            c: "Obi-Wan Kenobi", d: "X-Wing @Aliciousness"},
+        correctAnswer: ("c")
     },
     {
-        question: "Which"
+        question: "Who was born on the planet Titan?",
+        choices:{
+            a: "Thanos", b: "Jon Snow", c: "Ned Flanders", d:"Hercules"},
+        correctAnswer: ("a")
+    },
+    {
+        question: "Who's awesome?",
+        choices: {
+            a: "not me", b: "not you", c: "not I", d: "me"},
+        correctAnswer: ("d")
     }
-]
+];
